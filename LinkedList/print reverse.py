9c815d191797770396f9f1355469685a41691f98
avinash <dvv.avinash@gmail.com>
2017-03-28 21:36:25 +0530

    print revese

---
+++ LinkedList/print reverse.py
@@ -0,0 +1,28 @@
+"""
+ Print elements of a linked list in reverse order as standard output
+ head could be None as well for empty list
+ Node is defined as
+ 
+ class Node(object):
+ 
+   def __init__(self, data=None, next_node=None):
+       self.data = data
+       self.next = next_node
+
+ 
+"""
+
+def ReversePrint(head):
+    if(head != None):
+        lis = []
+        current = head;
+        lis.append(current.data)
+        while(current.next != None):
+            current = current.next
+            lis.append(current.data)
+        for i in range(len(lis)-1,-1,-1):
+            print(lis[i])
+  
+  
+    
+
